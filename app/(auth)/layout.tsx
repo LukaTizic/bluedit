5ae4620dd59880dc638b39ca491f7c5f54a332be
Luka <alutza94@gmail.com>
2025-05-07 15:36:10 +0200

    social auth form

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,26 @@
+import SocialAuthForm from "@/components/forms/SocialAuthForm";
 import React, { ReactNode } from "react";
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
-  return <main>{children}</main>;
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-auth-light dark:bg-auth-dark bg-cover bg-center bg-no-repeat px-5 py-10">
+      <section className="light-border background-light800_dark200 shadow-light-100_dark100 min-w-full rounded-[10px] border px-4 py-10 shadow-md sm:min-w-[520px] sm:px-8">
+        <div className="flex items-center justify-between gap-2">
+          <div className="space-y-2.5">
+            <h1 className="h2-bold text-dark-100_light900">
+              Join Blue<span className="text-primary-500">dit</span>
+            </h1>
+            <p className="paragraph-regular text-dark500_light400">
+              Help others and ask for help
+            </p>
+          </div>
+        </div>
+        {children}
+        {/* TODO: Add social authentication */}
+        <SocialAuthForm />
+      </section>
+    </main>
+  );
 };
 
 export default AuthLayout;
