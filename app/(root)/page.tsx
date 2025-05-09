c7fd1b256c88b6cf397fe9acfc3baf9f55d26d91
Luka <alutza94@gmail.com>
2025-05-09 13:48:22 +0200

    mobile navigation - left sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,5 @@
 // signout on server import
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
+import { auth } from "@/auth";
 import React from "react";
 
 const Home = async () => {
@@ -10,16 +8,6 @@ const Home = async () => {
   return (
     <>
       <h1 className="h1-bold text-white">Hello</h1>
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
-
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type="submit">Logout</Button>
-      </form>
     </>
   );
 };
