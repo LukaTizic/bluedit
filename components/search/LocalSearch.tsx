7ad3a8e05a598621898ebc19edbab82444c918a6
Luka <alutza94@gmail.com>
2025-05-16 16:19:04 +0200

    implementation of query string if (searchQuery)

--- components/search/LocalSearch.tsx
+++ components/search/LocalSearch.tsx
@@ -1,7 +1,9 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Input } from "../ui/input";
 import Image from "next/image";
+import { useSearchParams, useRouter } from "next/navigation";
+import { formUrlQuery } from "@/lib/url";
 
 interface Props {
   route: string;
@@ -11,10 +13,29 @@ interface Props {
 }
 
 const LocalSearch = ({ route, imgSrc, placeholder, otherClasses }: Props) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const query = searchParams.get("query") || "";
+
+  const [searchQuery, setSearchQuery] = useState(query);
+
+  useEffect(() => {
+    if (searchQuery) {
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "query",
+        value: searchQuery,
+      });
+      router.push(newUrl, { scroll: false });
+    } else {
+    }
+  }, [searchQuery, router, route, searchParams]);
+
   return (
     <div
       className={`background-light800_darkgradient flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4 ${otherClasses}`}
     >
+      {searchParams.toString()}
       <Image
         src={imgSrc}
         width={24}
@@ -25,8 +46,10 @@ const LocalSearch = ({ route, imgSrc, placeholder, otherClasses }: Props) => {
       <Input
         type="text"
         placeholder={placeholder}
-        value=""
-        onChange={() => {}}
+        value={searchQuery}
+        onChange={(e) => {
+          setSearchQuery(e.target.value);
+        }}
         className="paragraph-regular no-focus placeholder text-dark400_light700 border-none shadow-none outline-none "
       />
     </div>
