0b769e93a007c2458cb8137a48e03c8f394ea959
Luka <alutza94@gmail.com>
2025-05-21 17:52:00 +0200

    implement tags

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -1,4 +1,8 @@
+import ROUTES from "@/constants/routes";
+import { getTimeStamp } from "@/lib/utils";
+import Link from "next/link";
 import React from "react";
+import TagCard from "./TagCard";
 
 interface Props {
   question: Question;
@@ -8,8 +12,24 @@ const QuestionCard = ({
   question: { _id, title, tags, author, createdAt, upvotes, answers, views },
 }: Props) => {
   return (
-    <div>
-      <h3>{title}</h3>
+    <div className="card-wrapper rounded-[10px] p-9 sm:px-11">
+      <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
+        <div>
+          <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
+            {getTimeStamp(createdAt)}
+          </span>
+          <Link href={ROUTES.QUESTION(_id)}>
+            <h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1">
+              {title}
+            </h3>
+          </Link>
+        </div>
+      </div>
+      <div className="mt-3.5 flex w-full flex-wrap gap-2">
+        {tags.map((tag: Tag) => (
+          <TagCard key={tag._id} _id={tag._id} name={tag.name} compact />
+        ))}
+      </div>
     </div>
   );
 };
