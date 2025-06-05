6176b389ed4c1fb6c001ecb1343cfd92489d927d
Luka <alutza94@gmail.com>
2025-06-05 17:04:00 +0200

    Implement pino / pino pretty loggers

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,5 +1,6 @@
 import { error } from "console";
 import mongoose, { Mongoose } from "mongoose";
+import logger from "./logger";
 
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
@@ -27,6 +28,7 @@ if (!cached) {
 
 const dbConnect = async (): Promise<Mongoose> => {
   if (cached.conn) {
+    logger.info("Using existing mongoose connection");
     return cached.conn;
   }
 
@@ -36,11 +38,11 @@ const dbConnect = async (): Promise<Mongoose> => {
         dbName: "Bluedit",
       })
       .then((result) => {
-        console.log("Connected to MongoDB");
+        logger.info("Connected to MongoDB");
         return result;
       })
       .catch((error) => {
-        console.error("Error connecting to MongoDB", error);
+        logger.error("Error connecting to MongoDB", error);
         throw error;
       });
   }
