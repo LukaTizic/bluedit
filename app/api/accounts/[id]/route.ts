57ca8b8da35ec0ce050b40b9e37a5ea2bd4642e9
Luka <alutza94@gmail.com>
2025-06-11 13:25:29 +0200

    dynamic routes - delete account

--- app/api/accounts/[id]/route.ts
+++ app/api/accounts/[id]/route.ts
@@ -26,3 +26,23 @@ export async function GET(
     return handleError(error, "api") as APIErrorResponse;
   }
 }
+
+// DELETE /api/users/[id]
+export async function DELETE(
+  _: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  if (!id) throw new NotFoundError("Account");
+
+  try {
+    await dbConnect();
+
+    const account = await User.findByIdAndDelete(id);
+    if (!account) throw new NotFoundError("Account");
+
+    return NextResponse.json({ success: true, data: account }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
