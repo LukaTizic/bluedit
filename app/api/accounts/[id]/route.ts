765c7651293fde86a0cbc39cdccb23f24a243aa9
Luka <alutza94@gmail.com>
2025-06-11 13:28:39 +0200

    dynamic routes - create account

--- app/api/accounts/[id]/route.ts
+++ app/api/accounts/[id]/route.ts
@@ -46,3 +46,35 @@ export async function DELETE(
     return handleError(error, "api") as APIErrorResponse;
   }
 }
+
+// PUT /api/users/[id]
+export async function PUT(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  if (!id) throw new NotFoundError("Account");
+
+  try {
+    await dbConnect();
+
+    const body = await request.json();
+    const validatedData = AccountSchema.partial().safeParse(body);
+
+    if (!validatedData.success)
+      throw new ValidationError(validatedData.error.flatten().fieldErrors);
+
+    const updatedAccount = await Account.findByIdAndUpdate(id, validatedData, {
+      new: true,
+    });
+
+    if (!updatedAccount) throw new NotFoundError("Account");
+
+    return NextResponse.json(
+      { success: true, data: updatedAccount },
+      { status: 200 }
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
