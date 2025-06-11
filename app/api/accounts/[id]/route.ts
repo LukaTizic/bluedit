a1679c980e12a5fd0a6875ef14c13d6ee50189e3
Luka <alutza94@gmail.com>
2025-06-11 13:23:08 +0200

    dynamic routes - get all accounts

---
+++ app/api/accounts/[id]/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+
+import Account from "@/database/account.model";
+import handleError from "@/lib/handlers/error";
+import { ForbiddenError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
+import { AccountSchema } from "@/lib/validations";
+
+export async function GET() {
+  try {
+    await dbConnect();
+
+    const accounts = await Account.find();
+
+    return NextResponse.json(
+      { success: true, data: accounts },
+      { status: 200 }
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
