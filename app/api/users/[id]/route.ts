3a8a894cb73a5c29db3dcc4136d303bfc0bf431f
Luka <alutza94@gmail.com>
2025-06-11 14:07:13 +0200

    delete user route

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -22,3 +22,23 @@ export async function GET(
     return handleError(error, "api") as APIErrorResponse;
   }
 }
+
+// DELETE /api/users/[id]
+export async function DELETE(
+  _: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  if (!id) throw new NotFoundError("User");
+
+  try {
+    await dbConnect();
+
+    const user = await User.findByIdAndDelete(id);
+    if (!user) throw new NotFoundError("User");
+
+    return NextResponse.json({ success: true, data: user }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
