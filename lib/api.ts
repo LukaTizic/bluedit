7c91e097b2266739fb12aa7cf311268c8d860472
Luka <alutza94@gmail.com>
2025-09-16 23:57:51 +0200

    Log in for google/github during development

--- lib/api.ts
+++ lib/api.ts
@@ -4,8 +4,10 @@ import { IUser } from "@/database/user.model";
 
 import { fetchHandler } from "./handlers/fetch";
 
-const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api";
+// const API_BASE_URL =
+//   process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api";
+
+const API_BASE_URL = "http://localhost:3001/api";
 
 export const api = {
   auth: {
