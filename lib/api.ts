fb37587be0ed219e3eb64bf047dafded008ab414
Luka <alutza94@gmail.com>
2025-09-16 23:58:08 +0200

    TODO: FIX FOR PRODUCTION

--- lib/api.ts
+++ lib/api.ts
@@ -4,6 +4,7 @@ import { IUser } from "@/database/user.model";
 
 import { fetchHandler } from "./handlers/fetch";
 
+// TODO: FIX FOR PRODUCTION
 // const API_BASE_URL =
 //   process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api";
 
