1586dbdf18055a4a2bd4969d1af6db89c60b7dde
Luka <alutza94@gmail.com>
2025-10-06 18:04:21 +0200

    add hasSavedQuestion action // fix small error and add revalitadePath for UI

--- lib/actions/collection.action.ts
+++ lib/actions/collection.action.ts
@@ -35,7 +35,8 @@ export async function toggleSaveQuestion(
     });
 
     if (collection) {
-      await Collection.findByIdAndDelete(collection.id);
+      await Collection.findByIdAndDelete(collection._id);
+      revalidatePath(`/question/${questionId}`);
 
       return {
         success: true,
@@ -51,10 +52,44 @@ export async function toggleSaveQuestion(
     });
 
     revalidatePath(`/question/${questionId}`);
+
     return {
       success: true,
       data: {
         saved: true,
+      },
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
+
+export async function hasSavedQuestion(
+  params: CollectionBaseParams
+): Promise<ActionResponse<{ saved: boolean }>> {
+  const validationResult = await action({
+    params,
+    schema: CollectionBaseSchema,
+    authorize: true,
+  });
+
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  const { questionId } = validationResult.params!;
+  const userId = validationResult.session?.user?.id;
+
+  try {
+    const collection = await Collection.findOne({
+      question: questionId,
+      author: userId,
+    });
+
+    return {
+      success: true,
+      data: {
+        saved: !!collection,
       },
     };
   } catch (error) {
